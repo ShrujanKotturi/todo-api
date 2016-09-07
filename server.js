0624980ab664100120c82f6be20eea4e60d0aea9
Shrujan Kotturi <skotturi@uncc.edu>
2016-09-07 00:46:58 -0400

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Leave to mother land',
-    completed : false
-}, {
-    id: 2,
-    description: 'Call Rachel',
-    completed : false
-}, {
-    id: 3,
-    description: 'Call Selena',
-    completed : true
-}];
+// var todos = [{
+//     id: 1,
+//     description: 'Leave to mother land',
+//     completed : false
+// }, {
+//     id: 2,
+//     description: 'Call Rachel',
+//     completed : false
+// }, {
+//     id: 3,
+//     description: 'Call Selena',
+//     completed : true
+// }];
+
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send('Todo API root');
@@ -45,6 +52,18 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
     }
 });
+
+// POST /todos
+app.post('/todos', function (req, res){
+    var body = req.body;
+    body.id = todoNextId++;
+
+    todos.push(body);
+    console.log('description: ' + body.description);
+
+    res.json(body);
+});
+
 
 app.listen(PORT, function (){
    console.log('Listening on Port Number ' + PORT);
